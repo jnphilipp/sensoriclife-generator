82e23eb2cc06f3240fc75a92e3c1a891095fe062
Stefan Richter <Hewuri@googlemail.com>
2014-05-03 17:19:02 +0200

    adapted E-Generator to Spout interfaces

--- src/main/java/org/sensoriclife/generator/ElectricityGenerator.java
+++ src/main/java/org/sensoriclife/generator/ElectricityGenerator.java
@@ -2,71 +2,74 @@ package org.sensoriclife.generator;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Scanner; 
+import java.util.Map;
 import org.sensoriclife.Logger;
 import org.sensoriclife.util.Helpers;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
 
 /**
  * 
  * @author paul
  * @version 0.0.1
  */
-public class ElectricityGenerator implements Serializable
-{
+public class ElectricityGenerator extends BaseRichSpout {
 
-	private ArrayList<User> userList= new ArrayList<User>();
+	private ArrayList<User> userList = new ArrayList<User>();
 	private ArrayList<ResidentialUnit> residentialList = new ArrayList<ResidentialUnit>();
-	
-	public void run()
-	{
-		//read userList & residentialList-file and write this in the global variable
-		try
-		{
+	private SpoutOutputCollector collector;
+	private int timestamp = 0;
+
+	private void deserializing() {
+		try {
 			ObjectInputStream o1 = new ObjectInputStream(new FileInputStream(Helpers.getUserDir() + "/data/userList.ser"));
-			userList = (ArrayList<User>) o1.readObject();
+			this.userList = (ArrayList<User>) o1.readObject();
 			o1.close();
-			
+
 			ObjectInputStream o2 = new ObjectInputStream(new FileInputStream(Helpers.getUserDir() + "/data/residentialList.ser"));
-			residentialList	= (ArrayList<ResidentialUnit>) o2.readObject();
+			this.residentialList = (ArrayList<ResidentialUnit>) o2.readObject();
 			o2.close();
-		}
-		catch( Exception e ) 
-		{ 
+		} catch (Exception e) {
 			Logger.error(ElectricityGenerator.class, e.toString());
 		}
-		//send once a spout of the user information as jason
-		
-		int timestamp = 0;
-		while(true)//endless generaed
-		{
-			for(int i=0;i<residentialList.size();i++)
-			{
-			//send spout with following data
-				//id
-				Logger.info(ElectricityGenerator.class, "ElectricityID", ""+residentialList.get(i).getElectricityID() );
-				//electricity meter
-				double temp = residentialList.get(i).getElectricityMeter()+residentialList.get(i).getPersons()*0.2;//calculate new value with old value+persons*0,2
-				residentialList.get(i).setElectricityMeter( (int)temp );//set new value
-				Logger.info(ElectricityGenerator.class, "ElectricityMeter", ""+residentialList.get(i).getElectricityMeter() );
-				//timestamp
-				Logger.info(ElectricityGenerator.class, "ElectricityGeneratorTimestamp", ""+timestamp );
-			}
-			//count up timestamp
-			timestamp=timestamp+15;
-			//simulation time option
-			if(App.getBooleanProperty("realtime"))
-			{
-				try
-				{
-					Thread.sleep(900000);
-				}
-				catch( Exception e ) 
-				{ 
-					Logger.error(ElectricityGenerator.class, e.toString()); 
-				}
-			}	
+	}
+
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		this.collector = collector;
+	}
+
+	@Override
+	public void nextTuple() {
+		if (residentialList == null)
+			deserializing();
+
+		for (ResidentialUnit unit : residentialList) {
+			int val = unit.getElectricityMeter() + (int) (unit.getPersons() * 0.2);
+			unit.setElectricityMeter(val);
+			timestamp += 15;
+
+			this.collector.emit(new Values(unit.getElectricityID(), unit.getElectricityMeter(), timestamp));
 		}
+
+		if (App.getBooleanProperty("realtime")) {
+			try {
+				Thread.sleep(1000);// for testing only 1 sec
+				//Thread.sleep(900000);
+			} catch (Exception e) {
+				Logger.error(ElectricityGenerator.class, e.toString());
+			}
+		}
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("nextBolt")); // TODO: findname for nextBolt
 	}
 }
